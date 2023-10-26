a1fe03e70f49dce2959ef2b915a16e2737c69cb6
PrunierPro <prunierthomas.pro@gmail.com>
2023-10-26 11:53:55 +0200

    LoginRequestDTO

---
+++ FilRougeCore/DTOs/LoginRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilRougeCore.DTOs
+{
+    public class LoginRequestDTO
+    {
+        [Required]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email address")]
+        public string? Email { get; set; }
+        [Required]
+        //[PasswordValidator]
+        public string? PassWord { get; set; }
+    }
+}
